ebe697734797b48843a96c5bf3fa309dc4a83430
Aliakbar Huseynov <gsynvali@gmail.com>
2025-09-14 00:41:25 +0400

    updates

--- src/components/home/ElevenLabsInlineEmbed.tsx
+++ src/components/home/ElevenLabsInlineEmbed.tsx
@@ -1,6 +1,17 @@
 'use client'
 
 import { useEffect } from 'react'
+import type React from 'react'
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'elevenlabs-convai': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        [key: string]: any
+      }
+    }
+  }
+}
 
 export default function ElevenLabsInlineEmbed() {
   useEffect(() => {
