f659201b7c3c17dea2c6f15cae2f9a55637cb446
Aliakbar Huseynov <gsynvali@gmail.com>
2025-09-14 00:38:33 +0400

    changes

---
+++ src/types/jsx.ts
@@ -0,0 +1,15 @@
+import type React from 'react'
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'elevenlabs-convai': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        [key: string]: any
+      }
+    }
+  }
+}
+
+export {}
+
+
